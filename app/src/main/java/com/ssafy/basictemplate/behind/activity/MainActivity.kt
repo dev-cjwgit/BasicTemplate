dfa929ba3bae2b11b7015795796df0bde7098e44
최진우 <cjw_git@naver.com>
2023-05-12 23:05:19 +0900

    main activity 뒤로가기 시 바로 종료 방지

--- app/src/main/java/com/ssafy/basictemplate/behind/activity/MainActivity.kt
+++ app/src/main/java/com/ssafy/basictemplate/behind/activity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ssafy.basictemplate.behind.activity
 
 import android.os.Bundle
+import android.os.Process
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -11,7 +13,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ssafy.basictemplate.R
 import com.ssafy.basictemplate.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+    private var toast: Toast? = null
+    private var backKeyPressedTime: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding =
@@ -39,5 +44,22 @@ class MainActivity : AppCompatActivity() {
         }
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() > backKeyPressedTime + 2500) {
+            backKeyPressedTime = System.currentTimeMillis()
+            toast = Toast.makeText(this, "뒤로 가기 버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_LONG)
+            toast?.show()
+            return
+        }
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간에 2.5초를 더해 현재 시간과 비교 후
+        // 마지막으로 뒤로 가기 버튼을 눌렀던 시간이 2.5초가 지나지 않았으면 종료
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2500) {
+            Process.killProcess(Process.myPid())
+            toast?.cancel()
+            toast = Toast.makeText(this, "이용해 주셔서 감사합니다.", Toast.LENGTH_LONG)
+            toast?.show()
+        }
     }
 }
